4ae91b50c9ac43e3ddba9c06ea122d7bcc366830
Suprokash101 <Suprokash101@users.noreply.github.com>
2017-05-10 22:37:54 +0600

    Update second time gulpfile.js
    this update includes adding gulp-autoprefixer
    and gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-gulp.task('styles', function () {
-    gulp.src('./scss/main.scss')
+gulp.task('styles', function() {
+    gulp.src('./sass/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./css'))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('serve', function () {
+gulp.task('serve', function() {
     browserSync.init({
         server: {
             baseDir: './'
         }
-   });
+    });
 
-   gulp.watch('./scss/*.scss', ['styles']);
-   gulp.watch('./**/*.html').on('change', browserSync.reload);
+    gulp.watch('./sass/*.scss', ['styles']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
 
 });
 
