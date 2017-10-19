86c9486830005c0ce35152ab01fdc1df5d068393
Suprokash Chakraborty <Suprokash101@users.noreply.github.com>
2017-10-19 21:26:50 +0600

    set outputStyle of sass file as expanded

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,14 @@ var browserSync = require('browser-sync').create();
 gulp.task('styles', function() {
     gulp.src('./app/sass/main.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./app/css'))
         .pipe(browserSync.reload({ stream: true }));
 });
+
+/* outputStyle: 'compressed' or 'compact' or 'expanded' */
 
 gulp.task('serve', function() {
     browserSync.init({
